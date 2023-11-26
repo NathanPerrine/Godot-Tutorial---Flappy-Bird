2c83f35c4c3e76e94aa9b671147c133cbb14484c
Nathan <nathan.perrine@gmail.com>
2023-11-26 03:02:48 -0500

    collision detection on generated pipes

--- Bird.gd
+++ Bird.gd
@@ -1,8 +1,11 @@
 extends RigidBody2D
 
+@onready var pipes = $"../ParallaxBackground"
+var pipesList = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pipesList = pipes.pipesList
 	pass # Replace with function body.
 
 
@@ -17,10 +20,26 @@ func _input(event):
 			self.set_angular_velocity(-3)
 			$AnimatedSprite2D.play("Fly")
 
+## Called when hit detected, creates a 'bounce' and deletes the bird
+func _hit_detected(x, y):
+	self.set_linear_velocity(Vector2(x, y))
+	await get_tree().create_timer(0.25).timeout
+	self.queue_free()
+	return
 
 func _on_area_2d_body_entered(body):
-	if (body.name == ("Floor")):
-		self.set_linear_velocity(Vector2(0, -150))
-		await get_tree().create_timer(0.25).timeout
-		self.queue_free()
-		
+	print('body name: ' + body.name)
+	
+	match body.name:
+		"Floor":
+			_hit_detected(0, -150)
+
+		"Pipes":
+			_hit_detected(-150, 0)
+			
+	for pipe in pipesList:
+		if body.name in str(pipe):
+			_hit_detected(-150, 0)
+	
+	
+#
